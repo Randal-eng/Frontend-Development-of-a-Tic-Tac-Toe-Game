385a9cfb02bd7376bedecd50378039aa43a0063e
Randal <randal.ramirez4705@alumnos.udg.mx>
2024-05-31 12:05:37 -0600

    Logica de juego

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-let jugadorActual = 'X';
-let juegoActivo = true;
+let currentPlayer = 'X';
+let gameActive = true;
 const gameState = ['', '', '', '', '', '', '', '', ''];
 const winningConditions = [
     [0, 1, 2],
@@ -12,3 +12,63 @@ const winningConditions = [
     [2, 4, 6]
 ];
 
+const celdas = document.querySelectorAll('.celda');
+
+const handleCellClick = (event) => {
+    const clickedCell = event.target;
+    const clickedCellIndex = parseInt(clickedCell.getAttribute('index'));
+
+    if (gameState[clickedCellIndex] !== '' || !gameActive) {
+        return;
+    }
+
+    gameState[clickedCellIndex] = currentPlayer;
+    clickedCell.textContent = currentPlayer;
+
+    handleResultValidation();
+}
+
+const handleResultValidation = () => {
+    let roundWon = false;
+
+    for (const winCondition of winningConditions) {
+        const [a, b, c] = winCondition.map(index => gameState[index]);
+
+        if (a === '' || b === '' || c === '') {
+            continue;
+        }
+        if (a === b && b === c) {
+            roundWon = true;
+            break;
+        }
+    }
+
+    if (roundWon) {
+        gameActive = false;
+        alert(`¡Jugador ${currentPlayer} ha ganado!`);
+        return;
+    }
+
+    if (!gameState.includes('')) {
+        gameActive = false;
+        alert('¡Empate!');
+        return;
+    }
+
+    switchPlayer();
+}
+
+const switchPlayer = () => {
+    currentPlayer = currentPlayer === 'X' ? 'O' : 'X';
+}
+
+const restartGame = () => {
+    gameActive = true;
+    currentPlayer = 'X';
+    gameState.fill('');
+    celdas.forEach(cell => (cell.textContent = ''));
+}
+
+celdas.forEach(cell => cell.addEventListener('click', handleCellClick));
+document.getElementById('restartButton').addEventListener('click', restartGame);
+document.addEventListener('DOMContentLoaded', restartGame);
