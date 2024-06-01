5b5c971214c7ca974fd011ba907d4253b9277c7b
Randal <randal.ramirez4705@alumnos.udg.mx>
2024-05-31 20:04:19 -0600

    Sonidos

--- js/soundPlayer.js
+++ js/soundPlayer.js
@@ -1,25 +1,25 @@
 
 const playSoundX = () => {
-    const soundClicked = new Audio('./sounds/happy-pop-3-185288.mp3');
+    const soundClicked = new Audio('./sounds/happy-pop-3-185288.wav');
     soundClicked.play();
 }
 
 const playSoundO = () => {
-    const soundClicked = new Audio('./sounds/happy-pop-2-185287.mp3');
+    const soundClicked = new Audio('./sounds/happy-pop-2-185287.wav');
     soundClicked.play();
 }
 
 const playSoundRestar = () => {
-    const soundClicked = new Audio('./sounds/happy-pop-1-185286.mp3');
+    const soundClicked = new Audio('./sounds/happy-pop-1-185286.wav');
     soundClicked.play();
 }
 
 const playSoundWin = () =>{
-    const soundClicked = new Audio('./sounds/level-up-2-199574.mp3')
+    const soundClicked = new Audio('./sounds/level-up-2-199574.wav')
     soundClicked.play();
 }
 
 const playSoundEmpate = () =>{
-    const soundClicked = new Audio('./sounds/error-8-206492.mp3')
+    const soundClicked = new Audio('./sounds/error-8-206492.wav')
     soundClicked.play();
 }
